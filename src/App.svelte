2d8e0c33f49541afd05dc7cdaeba5a0e5e2d458a
Sita <sitavemuri8@gmail.com>
2025-07-21 15:25:17 -0700

    add conclusion component

--- src/App.svelte
+++ src/App.svelte
@@ -6,14 +6,16 @@
     import Introduction from "./lib/components/Introduction.svelte";
     import Card from "./lib/components/Card.svelte";
     import Map from "./lib/components/Map.svelte";
+    import Conclusion from "./lib/components/Conclusion.svelte";
 
     setContext("scrollama", scrollama);
     setContext("d3", d3);
 </script>
 
 <main>
-    <Hero title="Oakland" subtitle="Subtitle" />
+    <Hero title="Oakland Unscrolled" subtitle="Mapping Racial Inequity" />
     <Introduction />
     <Card text="But discriminatory policies existed all over the country, not least right here at home. Though they no longer exist on paper, they continue to impact access to wealth for families and communities. One practice is particularly visible in the Bay Area: " span="redlining" suffix="." />
     <Map />
+    <Conclusion />
 </main>
